79921dcc36dc5d3c911978a643b1f7856c2a38d0
Rafal-Dabek <62549937+Rafal-Dabek@users.noreply.github.com>
2021-11-16 23:06:47 +0100

    Beta version

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 
 function done(){
-  var mentor=0;
-  var coach=0;
-  var doradca=0;
+  var achivement=0;
+  var control=0;
+  var membership=0;
   var ele = document.getElementsByTagName('input');
     
 
@@ -21,7 +21,7 @@ function done(){
                 {
 
                   if(ele[i].value!=null)d+= ele[i].value ;
-                 
+                  
                 }
                     
 
@@ -33,28 +33,32 @@ function done(){
 
 
   var p=Array.from(d);
- 
-  
-coach=parseInt(p[2])+parseInt(p[4])+parseInt(p[7])+parseInt(p[9])+parseInt(p[14])+parseInt(p[15])+parseInt(p[18])+parseInt(p[22])+parseInt(p[25])+parseInt(p[28]);
-mentor=parseInt(p[0])+parseInt(p[5])+parseInt(p[6])+parseInt(p[11])+parseInt(p[12])+parseInt(p[17])+parseInt(p[20])+parseInt(p[23])+parseInt(p[24])+parseInt(p[29]);
-doradca=parseInt(p[1])+parseInt(p[3])+parseInt(p[8])+parseInt(p[10])+parseInt(p[13])+parseInt(p[16])+parseInt(p[19])+parseInt(p[21])+parseInt(p[26])+parseInt(p[27]);
-console.log(mentor)
+ for (i=0;i<15;i+=3)
+ {
 
-if(isNaN(coach) || isNaN(mentor) || isNaN(doradca))
+achivement+=parseInt(p[i]);
+control+=parseInt(p[i+1]);
+membership+=parseInt(p[i+2]);
+ }
+  
+
+
+
+if(achivement==0 &&control==0 && membership==0)
 {
   alert ("Wszystkie pola muszą być uzupełnione!");
 }
 else
 {
   document.getElementById("disp").innerHTML
-  +=  " Coach: "
-  + coach + "<br>"
+  +=  " Osiągnięcia: "
+  + achivement + "<br>"
   
-    + " Mentor: "
-  + mentor + "<br>"
+    + " Władza: "
+  + control + "<br>"
   
-   + " Doradca: "
-  + doradca + "<br>";
+   + " Przynależnośc: "
+  + membership + "<br>";
 }
 
 
